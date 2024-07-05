9f5716b5eb67479e2009964fca66922e41b0bd38
Allan <kiruiallan003@gmail.com>
2024-07-05 15:41:49 -0400

    Set up models: user, role, permission

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app import db as db
-from .role import Role as Role
-from .user import User as User
-from .batch import Batch as Batch
-from .permission import Permission as Permission
-from .transaction import Transaction as Transaction
-from .digital_certificate import DigitalCertificate as DigitalCertificate
